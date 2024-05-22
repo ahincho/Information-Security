220d0a1a5d2969976ac69f7baffe2edbbe00d60c
Fabiola Tapara <82911419+FabiTaparaQuispe@users.noreply.github.com>
2024-05-22 03:55:27 -0500

    We did the exercise one and two Caesar

---
+++ 02-cryptography/Python/Cesar/Exercise1-2/cesar.py
@@ -0,0 +1,23 @@
+from source import *
+from cesar_source import *
+
+
+def main():
+    # Getting file name
+    file_name = read_args2()
+
+    # Reading content
+    content = open_file(file_name)
+
+    # Cleaning the content
+    content = clean_content(content)
+
+    # Getting encode content (Cesar version)
+    displacement = 3
+    encoding_content = encode_cesar(content, displacement)
+
+    print(encoding_content)
+
+
+if __name__ == "__main__":
+    main()
