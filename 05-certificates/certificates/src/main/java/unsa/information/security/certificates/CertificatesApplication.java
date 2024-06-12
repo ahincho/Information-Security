d1fe7f4ac0ffa40361fc69f5b66fa0c5ed90d7ad
ahincho <ahincho@unsa.edu.pe>
2024-06-12 14:03:03 -0500

    Spring Application

--- 05-certificates/certificates/src/main/java/unsa/information/security/certificates/CertificatesApplication.java
+++ 05-certificates/certificates/src/main/java/unsa/information/security/certificates/CertificatesApplication.java
@@ -2,12 +2,11 @@ package unsa.information.security.certificates;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 public class CertificatesApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CertificatesApplication.class, args);
 	}
-
 }
