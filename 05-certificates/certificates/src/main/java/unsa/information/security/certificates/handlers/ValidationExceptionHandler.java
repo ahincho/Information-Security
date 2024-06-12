007133ec39bb93604624d1587b764a6cb55a9069
ahincho <ahincho@unsa.edu.pe>
2024-06-12 13:59:20 -0500

    Exception Handlers

---
+++ 05-certificates/certificates/src/main/java/unsa/information/security/certificates/handlers/ValidationExceptionHandler.java
@@ -0,0 +1,30 @@
+package unsa.information.security.certificates.handlers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.util.List;
+
+@RestControllerAdvice
+public class ValidationExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<NotValidField>> notValidEntityOrRequest(MethodArgumentNotValidException notValidException) {
+        var errors = notValidException.getFieldErrors().stream().map(NotValidField::new).toList();
+        return ResponseEntity.badRequest().body(errors);
+    }
+    @ExceptionHandler({MethodArgumentTypeMismatchException.class, HttpMessageNotReadableException.class})
+    public ResponseEntity<Void> badRequest() {
+        return ResponseEntity.badRequest().build();
+    }
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<Void> notImplemented() {
+        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
+    }
+}
